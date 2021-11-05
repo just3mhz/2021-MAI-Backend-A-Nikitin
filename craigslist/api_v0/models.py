850675d99625050694f080ec3469108ffcd8be1b
just3mhz <andrey.nikitin.98@yandex.ru>
2021-11-05 19:04:35 +0300

    Add __str__ methods to model

--- craigslist/api_v0/models.py
+++ craigslist/api_v0/models.py
@@ -8,11 +8,17 @@ class User(models.Model):
     phone = models.CharField(max_length=20)
     reg_date = models.DateField()
 
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
 
     category = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.category
 
 
 class Advertisement(models.Model):
@@ -25,3 +31,6 @@ class Advertisement(models.Model):
     description = models.CharField(max_length=600)
     pub_date = models.DateField()
     published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Объявление №{self.advertisement_id}'
